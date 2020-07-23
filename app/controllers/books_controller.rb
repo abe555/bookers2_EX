1c38069d5416513a44fd4924b7e3f1de98cd6cea
abe555 <riho.has3v@gmail.com>
2020-07-24 04:25:12 +0900

    Update [Edit & Destroy]_btn

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,9 +28,20 @@ class BooksController < ApplicationController
 	end
 
 	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:notice] = "Book was successfully updated!"
+			redirect_to book_path(@book.id)
+		else
+			flash[:notice] = "error!"
+			render :edit
+		end
 	end
 
 	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
 	end
 
 	private
