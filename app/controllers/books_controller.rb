99bc2a1493b881a696b1f4066462983061c99467
abe555 <riho.has3v@gmail.com>
2020-07-24 15:53:07 +0900

    Add CreateBook_function to /users/()

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
 	end
 
 	def create
-		@user.books = current_user.books.new(book_params)
+		@book = Book.new(book_params)
 		@book.user_id = current_user.id
 		@book.save
 		redirect_to book_path(@book.id)
